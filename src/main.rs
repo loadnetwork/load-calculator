4f8df72e988f84b0561e4eec08cd50e5896dd3e3
charmful0x <charmful0x@gmail.com>
2025-03-21 14:58:08 +0100

    feat: add cors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use crate::utils::server::{
     get_price_avail, get_price_babe, get_price_baselayer, get_price_celestia, root,
 };
-use axum::{Router, routing::get};
-use std::time::Duration;
-use tower_http::timeout::TimeoutLayer;
 
-use crate::utils::avail::cost_calculator;
+use axum::{
+    Router,
+    http::{Method, header},
+    routing::get,
+};
+use std::time::Duration;
+use tower_http::cors::CorsLayer;
+use tower_http::timeout::TimeoutLayer;
 
 pub mod utils;
 
@@ -13,8 +17,10 @@ pub mod utils;
 async fn main() -> shuttle_axum::ShuttleAxum {
     let timeout_layer = TimeoutLayer::new(Duration::from_secs(10));
 
-    let cost = cost_calculator(2621440).await.unwrap();
-    println!("{:?}", cost);
+    let cors = CorsLayer::new()
+        .allow_origin(tower_http::cors::Any)
+        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+        .allow_headers([header::CONTENT_TYPE, header::AUTHORIZATION, header::ACCEPT]);
 
     // server routes
     let router = Router::new()
@@ -27,7 +33,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/v1/babe/:size", get(get_price_babe))
         .route("/v1/celestia/:size", get(get_price_celestia))
         .route("/v1/avail/:size", get(get_price_avail))
-        .layer(timeout_layer);
+        .layer(timeout_layer)
+        .layer(cors);
 
     Ok(router.into())
 }
